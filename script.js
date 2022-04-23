fc2e2deaabe804e6ce6bfa82893363f8862096e5
JayB <ajiboyeabiodun001@gmail.com>
2022-04-23 15:54:10 +0200

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let popupBox = document.getElementById('popup-box');
+let notes;
+let popupBox = document.getElementById('popup-box'),
+    noteTitle = document.getElementById('title'),
+    noteDescription = document.getElementById('description');
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
     function createNote() {
         popupBox.classList.remove('visible');
@@ -6,4 +10,24 @@ let popupBox = document.getElementById('popup-box');
 
     function closePopup() {
         popupBox.classList.add('visible');
+    }
+
+    function addNote() {
+        notes.push({title: noteTitle.value,
+            description: noteDescription.value,
+            date: months[new Date().getMonth()] + " " + new Date().getDate() + " " + new Date().getFullYear()});   
+            
+        saveToLocalStorage();
+    }
+
+    function saveToLocalStorage() {
+        localStorage.setItem("letter", JSON.stringify(notes));
+    }
+
+    let noteToGet = JSON.parse(localStorage.getItem("letter"))
+
+    if(Array.isArray(noteToGet)) {
+        notes = noteToGet;
+    } else{
+        notes = [];
     }
