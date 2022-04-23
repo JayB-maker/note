f3ca6e9d995775a682862db93a0869568bcba9e4
JayB <ajiboyeabiodun001@gmail.com>
2022-04-23 14:26:29 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let popupBox = document.getElementById('popup-box');
+
+    function createNote() {
+        popupBox.classList.remove('visible');
+    }
+
+    function closePopup() {
+        popupBox.classList.add('visible');
+    }
